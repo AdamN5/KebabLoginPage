6da2fa08b4fdd5e5e54e43f72621318a60ca5350
AdamNafea-STUDENT <g00462155@atu.ie>
2026-02-18 15:42:00 +0000

    Added html wireframe login

---
+++ src/main/java/ie/atu/kebabeloginwebpage/webController/WebController.java
@@ -0,0 +1,13 @@
+package ie.atu.kebabeloginwebpage.webController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class WebController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
