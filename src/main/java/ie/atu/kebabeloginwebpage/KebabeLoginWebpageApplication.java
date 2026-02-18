fcc1ce44a2cae0b340993dc5be73ddff96e4c465
AdamNafea-STUDENT <g00462155@atu.ie>
2026-02-18 15:33:05 +0000

    Added basic files to start project

---
+++ src/main/java/ie/atu/kebabeloginwebpage/KebabeLoginWebpageApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.kebabeloginwebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KebabeLoginWebpageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KebabeLoginWebpageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/kebabeloginwebpage/KebabeLoginWebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.kebabeloginwebpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KebabeLoginWebpageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
